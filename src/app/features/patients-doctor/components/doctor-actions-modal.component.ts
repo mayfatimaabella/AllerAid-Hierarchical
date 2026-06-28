54b8273bac537f226834a24eee30895adbcf21c9
PatDep-1 <patricia.deiparine.22@usjr.edu.ph>
2026-06-28 10:28:08 +0800

    6/28
    REMOVED:
    Patients-Doctor:
    edit function

--- src/app/features/patients-doctor/components/doctor-actions-modal.component.ts
+++ src/app/features/patients-doctor/components/doctor-actions-modal.component.ts
@@ -8,13 +8,8 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 })
 export class DoctorActionsModalComponent {
   @Input() doctor: any;
-  @Output() edit = new EventEmitter<any>();
   @Output() delete = new EventEmitter<any>();
   @Output() closeModal = new EventEmitter<void>();
-
-  editDoctor() {
-    this.edit.emit(this.doctor);
-  }
 
   deleteDoctor() {
     this.delete.emit(this.doctor);
